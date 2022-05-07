c1ec00686524a95006220307bcccc8d7eb4d44e6
VivianaMurillom <viviana.zs104@gmail.com>
2022-05-06 20:02:31 -0500

    Finish the first activity in project.js

---
+++ assets/js/project.js
@@ -0,0 +1,26 @@
+// Punto 1
+
+const userName='Admin';
+const userPasword='Kuepa2022';
+
+let buttonLogin = document.querySelector('.ingresar');
+
+buttonLogin.addEventListener('click', enviar=(e)=>{
+    e.preventDefault();
+
+    let dataUserName=document.getElementById('user').value;
+    let dataUserPassword=document.getElementById('password').value;
+
+    if (userName===dataUserName && userPasword===dataUserPassword) {
+        window.setTimeout(()=>{window.location.href='dashboard.html'},2000);
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: '¡No es posible iniciar sesión!',
+          })
+    }
+});
+
+// Punto 2
+
