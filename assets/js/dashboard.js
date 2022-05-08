bef4d43ed3450a81f2e696f07684e0e2f0463aab
VivianaMurillom <viviana.zs104@gmail.com>
2022-05-07 23:03:30 -0500

    renaming all clases and ids in dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -5,7 +5,7 @@ const user2=1001235478;
 const user3=25413574;
 const user4=1007203506;
 
-let buttonSearchU=document.querySelector('.buscar');
+let buttonSearchU=document.querySelector('.search');
 
 buttonSearchU.addEventListener('click', buscarDocument=(e)=>{
     e.preventDefault();
@@ -13,22 +13,22 @@ buttonSearchU.addEventListener('click', buscarDocument=(e)=>{
     let dataUserDocument=parseInt(document.getElementById('document').value);
 
     if (dataUserDocument===user1) {
-        var elemento=document.querySelectorAll('.registro1');
+        var elemento=document.querySelectorAll('.first-document');
         for (var i = 0; i<elemento.length; i++){
             elemento[i].classList.add('active');
         }
     } else if (dataUserDocument===user2) {
-        var elemento=document.querySelectorAll('.registro2');
+        var elemento=document.querySelectorAll('.second-document');
         for (var i = 0; i<elemento.length; i++){
             elemento[i].classList.add('active');
         }
     } else if (dataUserDocument===user3) {
-        var elemento=document.querySelectorAll('.registro3');
+        var elemento=document.querySelectorAll('.third-document');
         for (var i = 0; i<elemento.length; i++){
             elemento[i].classList.add('active');
         }
     } else if (dataUserDocument===user4) {
-        var elemento=document.querySelectorAll('.registro4');
+        var elemento=document.querySelectorAll('.fourth-document');
         for (var i = 0; i<elemento.length; i++){
             elemento[i].classList.add('active');
         }
